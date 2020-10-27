b3cb2c9736e86bb70ef50ee720cc64195cb359a2
aadarshjha <jha.aadarsh@gmail.com>
2020-10-26 21:35:00 -0500

    vtk example on button click

--- interface/main.py
+++ interface/main.py
@@ -1,27 +1,91 @@
 import tkinter as tk
+import vtk
+from vtk.tk.vtkTkRenderWindowInteractor import vtkTkRenderWindowInteractor
+from vtk.util.colors import tomato
+
 
 class Application(tk.Frame):
     def __init__(self, master=None):
         super().__init__(master)
         self.master = master
         self.pack()
-        self.create_widgets()
+        self.create_buttons()
+        self.create_vtk_view()
+        self.winfo_toplevel().title("TARA")
 
-    def create_widgets(self):
-        self.hi_there = tk.Button(self)
-        self.hi_there["text"] = "Hello World\n(click me)"
-        self.hi_there["command"] = self.say_hi
-        self.hi_there.pack(side="top")
 
-        self.quit = tk.Button(self, text="QUIT", fg="red",
-                              command=self.master.destroy)
-        self.quit.pack(side="bottom")
+    def create_vtk_view(self):
+        # here, we display a basic vtk screen
+        # as a result of pressing a button.
 
-    def say_hi(self):
-        print("hi there, everyone!")
+        # this just displays a random object.
+        # This creates a polygonal cylinder model with eight circumferential
+        # facets.
+        cylinder = vtk.vtkCylinderSource()
+        cylinder.SetResolution(8)
+
+        # The mapper is responsible for pushing the geometry into the graphics
+        # library. It may also do color mapping, if scalars or other
+        # attributes are defined.
+        cylinderMapper = vtk.vtkPolyDataMapper()
+        cylinderMapper.SetInputConnection(cylinder.GetOutputPort())
+
+        # The actor is a grouping mechanism: besides the geometry (mapper), it
+        # also has a property, transformation matrix, and/or texture map.
+        # Here we set its color and rotate it -22.5 degrees.
+        cylinderActor = vtk.vtkActor()
+        cylinderActor.SetMapper(cylinderMapper)
+        cylinderActor.GetProperty().SetColor(tomato)
+        cylinderActor.RotateX(30.0)
+        cylinderActor.RotateY(-45.0)
+
+        # Create the graphics structure. The renderer renders into the render
+        # window. The render window interactor captures mouse events and will
+        # perform appropriate camera or actor manipulation depending on the
+        # nature of the events.
+        ren = vtk.vtkRenderer()
+        renWin = vtk.vtkRenderWindow()
+        renWin.AddRenderer(ren)
+        iren = vtk.vtkRenderWindowInteractor()
+        iren.SetRenderWindow(renWin)
+
+        # Add the actors to the renderer, set the background and size
+        ren.AddActor(cylinderActor)
+        ren.SetBackground(0.1, 0.2, 0.4)
+        renWin.SetSize(200, 200)
+
+        # This allows the interactor to initalize itself. It has to be
+        # called before an event loop.
+        iren.Initialize()
+
+        # We'll zoom in a little by accessing the camera and invoking a "Zoom"
+        # method on it.
+        ren.ResetCamera()
+        ren.GetActiveCamera().Zoom(1.5)
+        renWin.Render()
+
+        # Start the event loop.
+        iren.Start()
+
+
+    def create_buttons(self):
+        # here we create a button to display the vtk screen
+        frame = tk.Frame(root)
+        frame.pack()
+        button = tk.Button(frame,
+                           text="Display VTK Screen",
+                           fg="black",
+                           command=self.create_vtk_view)
+        button.pack(side=tk.LEFT)
+
+
+
+def callback(event):
+    print("clicked at", event.x, event.y)
 
 root = tk.Tk()
 root.geometry("1020x700")
 root.resizable(False, False)
+
 app = Application(master=root)
 app.mainloop()
