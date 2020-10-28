24763e6f779dcee72390b1456749537e5dbd856b
raahul1705 <raahul1705@users.noreply.github.com>
2020-10-27 19:09:41 -0700

    Setup PyQT for GUI

--- interface/main.py
+++ interface/main.py
@@ -1,89 +1,68 @@
-import tkinter as tk
+import sys
 import vtk
-from vtk.tk.vtkTkRenderWindowInteractor import vtkTkRenderWindowInteractor
-from vtk.util.colors import tomato
+from PyQt5 import QtCore, QtWidgets
+from vtk.qt.QVTKRenderWindowInteractor import QVTKRenderWindowInteractor
+ 
+class MainWindow(QtWidgets.QMainWindow):
+ 
+    def __init__(self, parent = None):
+        QtWidgets.QMainWindow.__init__(self, parent)
+ 
+        self.frame = QtWidgets.QFrame()
+ 
+        self.main_layout = QtWidgets.QHBoxLayout()
+
+        # Add menu widgets
+        self.menu_layout = QtWidgets.QVBoxLayout()
+        self.button1 = QtWidgets.QPushButton('Button1', self)
+        self.button2 = QtWidgets.QPushButton('Button2', self)
+        self.button1.clicked.connect(lambda: self.printOut(self.button1.text()))
+        self.button2.clicked.connect(lambda: self.printOut(self.button2.text()))
+
+        self.menu_layout.addWidget(self.button1)
+        self.menu_layout.addWidget(self.button2)
 
 
-class Application(tk.Frame):
-    def __init__(self, master=None):
-        super().__init__(master)
-        self.master = master
-        self.pack()
-        self.create_buttons()
-        self.create_vtk_view()
-        self.winfo_toplevel().title("TARA")
+        self.main_layout.addLayout(self.menu_layout)
 
+        # VTK Widget
+        self.vtkWidget = QVTKRenderWindowInteractor(self.frame)
+        self.main_layout.addWidget(self.vtkWidget)
+ 
+        self.ren = vtk.vtkRenderer()
+        self.vtkWidget.GetRenderWindow().AddRenderer(self.ren)
+        self.iren = self.vtkWidget.GetRenderWindow().GetInteractor()
+ 
+        # Create source
+        source = vtk.vtkSphereSource()
+        source.SetCenter(0, 0, 0)
+        source.SetRadius(5.0)
+ 
+        # Create a mapper
+        mapper = vtk.vtkPolyDataMapper()
+        mapper.SetInputConnection(source.GetOutputPort())
+ 
+        # Create an actor
+        actor = vtk.vtkActor()
+        actor.SetMapper(mapper)
+ 
+        self.ren.AddActor(actor)
+ 
+        self.ren.ResetCamera()
+ 
+        self.frame.setLayout(self.main_layout)
+        self.setCentralWidget(self.frame)
 
-    def create_vtk_view(self):
-        # here, we display a basic vtk screen
-        # as a result of pressing a button.
+        self.show()
+        self.iren.Initialize()
 
-        # this just displays a random object.
-        # This creates a polygonal cylinder model with eight circumferential
-        # facets.
-        cylinder = vtk.vtkCylinderSource()
-        cylinder.SetResolution(8)
-
-        # The mapper is responsible for pushing the geometry into the graphics
-        # library. It may also do color mapping, if scalars or other
-        # attributes are defined.
-        cylinderMapper = vtk.vtkPolyDataMapper()
-        cylinderMapper.SetInputConnection(cylinder.GetOutputPort())
-
-        # The actor is a grouping mechanism: besides the geometry (mapper), it
-        # also has a property, transformation matrix, and/or texture map.
-        # Here we set its color and rotate it -22.5 degrees.
-        cylinderActor = vtk.vtkActor()
-        cylinderActor.SetMapper(cylinderMapper)
-        cylinderActor.GetProperty().SetColor(tomato)
-        cylinderActor.RotateX(30.0)
-        cylinderActor.RotateY(-45.0)
-
-        # Create the graphics structure. The renderer renders into the render
-        # window. The render window interactor captures mouse events and will
-        # perform appropriate camera or actor manipulation depending on the
-        # nature of the events.
-        ren = vtk.vtkRenderer()
-        renWin = vtk.vtkRenderWindow()
-        renWin.AddRenderer(ren)
-        iren = vtk.vtkRenderWindowInteractor()
-        iren.SetRenderWindow(renWin)
-
-        # Add the actors to the renderer, set the background and size
-        ren.AddActor(cylinderActor)
-        ren.SetBackground(0.1, 0.2, 0.4)
-        renWin.SetSize(200, 200)
-
-        # This allows the interactor to initalize itself. It has to be
-        # called before an event loop.
-        iren.Initialize()
-
-        # We'll zoom in a little by accessing the camera and invoking a "Zoom"
-        # method on it.
-        ren.ResetCamera()
-        ren.GetActiveCamera().Zoom(1.5)
-        renWin.Render()
-
-        # Start the event loop.
-        iren.Start()
-
-
-    def create_buttons(self):
-        # here we create a button to display the vtk screen
-        frame = tk.Frame(root)
-        frame.pack()
-        button = tk.Button(frame,
-                           text="Display VTK Screen",
-                           fg="black",
-                           command=self.create_vtk_view)
-        button.pack(side=tk.LEFT)
-
-def callback(event):
-    print("clicked at", event.x, event.y)
-
-root = tk.Tk()
-root.geometry("1020x700")
-root.resizable(False, False)
-
-app = Application(master=root)
-app.mainloop()
+    def printOut(self, msg):
+        print(msg)
+ 
+if __name__ == "__main__":
+ 
+    app = QtWidgets.QApplication(sys.argv)
+ 
+    window = MainWindow()
+ 
+    sys.exit(app.exec_())
