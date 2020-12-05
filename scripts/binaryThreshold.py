b706b87408c2b87d52294ef35b4922578256fa4f
aadarshjha <jha.aadarsh@gmail.com>
2020-12-05 17:52:51 -0600

    a merge of some sort

--- scripts/binaryThreshold.py
+++ scripts/binaryThreshold.py
@@ -8,33 +8,36 @@ Ex Input: binaryThreshold.py 1000_3.nii.gz 1000_3_threshold.nii.gz 600 1500 0 1
 import sys
 import itk
 
-#Make sure input is selected
-if len(sys.argv) != 7:
-    print("Usage: " + sys.argv[0] + " <inputImage> <outputImage> "
-          "<lowerThreshold> <upperThreshold> <outsideValue> <insideValue>")
-    sys.exit(1)
+def arg_func(): 
+    #Make sure input is selected
+    if len(sys.argv) != 7:
+        print("Usage: " + sys.argv[0] + " <inputImage> <outputImage> "
+            "<lowerThreshold> <upperThreshold> <outsideValue> <insideValue>")
+        sys.exit(1)
 
-#Inputting the variables
-inputImage = "../data/input/" + sys.argv[1]
-outputImage = "../data/results/" + sys.argv[2]
-lowerThreshold = int(sys.argv[3])
-upperThreshold = int(sys.argv[4])
-outsideValue = int(sys.argv[5])
-insideValue = int(sys.argv[6])
+    #Inputting the variables
+    inputImage = "../data/input/" + sys.argv[1]
+    outputImage = "../data/results/" + sys.argv[2]
+    lowerThreshold = int(sys.argv[3])
+    upperThreshold = int(sys.argv[4])
+    outsideValue = int(sys.argv[5])
+    insideValue = int(sys.argv[6])
 
-#set up a reader
-reader = itk.ImageFileReader.New(FileName=inputImage)
+    #set up a reader
+    reader = itk.ImageFileReader.New(FileName=inputImage)
 
-#Create threshold filter
-thresholdFilter = itk.BinaryThresholdImageFilter.New(Input=reader.GetOutput(),
-                                                     LowerThreshold=lowerThreshold,
-                                                     UpperThreshold=upperThreshold,
-                                                     OutsideValue=outsideValue,
-                                                     InsideValue=insideValue)
+    #Create threshold filter
+    thresholdFilter = itk.BinaryThresholdImageFilter.New(Input=reader.GetOutput(),
+                                                        LowerThreshold=lowerThreshold,
+                                                        UpperThreshold=upperThreshold,
+                                                        OutsideValue=outsideValue,
+                                                        InsideValue=insideValue)
 
-#Write the file to an output image
-writer = itk.ImageFileWriter.New(FileName=outputImage, Input=thresholdFilter.GetOutput())
+    #Write the file to an output image
+    writer = itk.ImageFileWriter.New(FileName=outputImage, Input=thresholdFilter.GetOutput())
 
-#Actually write out the file
-writer.Update()
+    #Actually write out the file
+    writer.Update()
 
+if __name__ == '__main__':
+    arg_func()
