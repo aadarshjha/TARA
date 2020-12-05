213994ce2c4e3036e0341f521046d6e548eb9b3c
rxn <raahul1705@gmail.com>
2020-12-05 14:34:32 -0800

    Added file selection

--- interface/main.py
+++ interface/main.py
@@ -2,12 +2,12 @@ import sys
 import vtk
 from PyQt5 import QtCore, QtWidgets
 from vtk.qt.QVTKRenderWindowInteractor import QVTKRenderWindowInteractor
- 
+from PyQt5.QtWidgets import QFileDialog
+
 class MainWindow(QtWidgets.QMainWindow):
- 
     def __init__(self, parent = None):
         QtWidgets.QMainWindow.__init__(self, parent)
- 
+        self.renderers = []
         self.frame = QtWidgets.QFrame()
         self.setFixedHeight(520)
         self.setFixedWidth(900)
@@ -16,9 +16,9 @@ class MainWindow(QtWidgets.QMainWindow):
 
         # Add menu widgets
         self.menu_layout = QtWidgets.QVBoxLayout()
-        self.button1 = QtWidgets.QPushButton('Button1', self)
+        self.button1 = QtWidgets.QPushButton('Open NIFTI', self)
         self.button2 = QtWidgets.QPushButton('Button2', self)
-        self.button1.clicked.connect(lambda: self.printOut(self.button1.text()))
+        self.button1.clicked.connect(lambda: self.openFileNameDialog())
         self.button2.clicked.connect(lambda: self.printOut(self.button2.text()))
 
         self.menu_layout.addWidget(self.button1)
@@ -31,26 +31,12 @@ class MainWindow(QtWidgets.QMainWindow):
         self.vtkWidget = QVTKRenderWindowInteractor(self.frame)
         self.main_layout.addWidget(self.vtkWidget)
  
-        self.ren = vtk.vtkRenderer()
-        self.vtkWidget.GetRenderWindow().AddRenderer(self.ren)
+        ren = vtk.vtkRenderer()
+        self.renderers.append(ren)
+        self.vtkWidget.GetRenderWindow().AddRenderer(ren)
         self.iren = self.vtkWidget.GetRenderWindow().GetInteractor()
  
-        # Create source
-        source = vtk.vtkSphereSource()
-        source.SetCenter(0, 0, 0)
-        source.SetRadius(5.0)
- 
-        # Create a mapper
-        mapper = vtk.vtkPolyDataMapper()
-        mapper.SetInputConnection(source.GetOutputPort())
- 
-        # Create an actor
-        actor = vtk.vtkActor()
-        actor.SetMapper(mapper)
- 
-        self.ren.AddActor(actor)
- 
-        self.ren.ResetCamera()
+        ren.ResetCamera()
  
         self.frame.setLayout(self.main_layout)
         self.setCentralWidget(self.frame)
@@ -58,9 +44,56 @@ class MainWindow(QtWidgets.QMainWindow):
         self.show()
         self.iren.Initialize()
 
+    def openFileNameDialog(self):
+        options = QFileDialog.Options()
+        options |= QFileDialog.DontUseNativeDialog
+        file_name, _ = QFileDialog.getOpenFileName(self, 
+            "QFileDialog.getOpenFileName()", 
+            "","NIFTI files (*.nii, *.nii.gz)", 
+            options=options)
+
+        if file_name:
+            print(file_name)
+            self.openImage(file_name)
+
     def printOut(self, msg):
         print(msg)
- 
+
+    def openImage(self, file_name):
+        reader = vtk.vtkNIFTIImageReader()
+        reader.SetFileName(file_name)
+        reader.Update()
+
+        scalarRange = reader.GetOutput().GetScalarRange()
+        extent = reader.GetOutput().GetExtent()
+        viewport = [[0.67, 0.0, 1.0, 0.5],
+                    [0.67, 0.5, 1.0, 1.0],
+                    [0.0, 0.0, 0.67, 1.0]]
+
+        for ren in self.renderers:
+            self.vtkWidget.GetRenderWindow().RemoveRenderer(ren)
+
+        for i in range(3):
+            mapper = vtk.vtkImageSliceMapper()
+            mapper.SetInputConnection(reader.GetOutputPort())
+            mapper.SetOrientation(i % 3)
+            mapper.SliceAtFocalPointOn()
+
+            image = vtk.vtkImageSlice()
+            image.SetMapper(mapper)
+            image.GetProperty().SetColorWindow(scalarRange[1] - scalarRange[0])
+            image.GetProperty().SetColorLevel(0.5 * (scalarRange[0] + scalarRange[1]))
+
+            renderer = vtk.vtkRenderer()
+            renderer.AddActor(image)
+            renderer.SetBackground(0, 0, 0)
+            self.vtkWidget.GetRenderWindow().AddRenderer(renderer)
+            self.renderers.append(renderer)
+
+            
+
+        self.iren.Initialize()
+
 if __name__ == "__main__":
  
     app = QtWidgets.QApplication(sys.argv)
