5d94494697487376a175071d04fe194194756464
hbnation <luo.terry@gmail.com>
2020-12-05 20:05:40 -0500

    add function encapsulation to ants scripts

--- scripts/deepAtropos.py
+++ scripts/deepAtropos.py
@@ -12,31 +12,34 @@ import sys
 import ants
 import antspynet
 
-#Make sure input is selected
-if len(sys.argv) != 10:
-    print("Usage: " + sys.argv[0] + " <inputImage> <Segmentation>"
-          "<BackgroundOutput> <CSFOutput> <GMOutput> <WMOutput> <DeepGMOutput> <BrainStem> <Cerebellum>")
-    sys.exit(1)
+def arg_func(args):
+    #Make sure input is selected
+    if len(sys.argv) != 10:
+        print("Usage: " + sys.argv[0] + " <inputImage> <Segmentation>"
+              "<BackgroundOutput> <CSFOutput> <GMOutput> <WMOutput> <DeepGMOutput> <BrainStem> <Cerebellum>")
+        sys.exit(1)
 
-inputImage = "../data/input/" + sys.argv[1]
-segmentationOutput = "../data/results/" + sys.argv[2]
-BackgroundOutput = "../data/results/" + sys.argv[3]
-CSFOutput = "../data/results/" + sys.argv[4]
-GMOutput = "../data/results/" + sys.argv[5]
-WMOutput = "../data/results/" + sys.argv[6]
-DeepGMOutput = "../data/results/" + sys.argv[7]
-BrainStemOutput = "../data/results/" + sys.argv[8]
-CerebellumOutput = "../data/results/" + sys.argv[9]
+    inputImage = "../data/input/" + sys.argv[1]
+    segmentationOutput = "../data/results/" + sys.argv[2]
+    BackgroundOutput = "../data/results/" + sys.argv[3]
+    CSFOutput = "../data/results/" + sys.argv[4]
+    GMOutput = "../data/results/" + sys.argv[5]
+    WMOutput = "../data/results/" + sys.argv[6]
+    DeepGMOutput = "../data/results/" + sys.argv[7]
+    BrainStemOutput = "../data/results/" + sys.argv[8]
+    CerebellumOutput = "../data/results/" + sys.argv[9]
 
-image = ants.image_read(inputImage)
-flash = antspynet.utilities.deep_atropos(image, do_preprocessing=True, verbose=True)
-ants.image_write(flash['segmentation_image'], segmentationOutput)
-probability = flash['probability_images']
-ants.image_write(probability[0], BackgroundOutput)
-ants.image_write(probability[1], CSFOutput)
-ants.image_write(probability[2], GMOutput)
-ants.image_write(probability[3], WMOutput)
-ants.image_write(probability[4], DeepGMOutput)
-ants.image_write(probability[5], BrainStemOutput)
-ants.image_write(probability[6], CerebellumOutput)
+    image = ants.image_read(inputImage)
+    flash = antspynet.utilities.deep_atropos(image, do_preprocessing=True, verbose=True)
+    ants.image_write(flash['segmentation_image'], segmentationOutput)
+    probability = flash['probability_images']
+    ants.image_write(probability[0], BackgroundOutput)
+    ants.image_write(probability[1], CSFOutput)
+    ants.image_write(probability[2], GMOutput)
+    ants.image_write(probability[3], WMOutput)
+    ants.image_write(probability[4], DeepGMOutput)
+    ants.image_write(probability[5], BrainStemOutput)
+    ants.image_write(probability[6], CerebellumOutput)
 
+if __name__ == '__main__':
+    arg_func(sys.argv)
